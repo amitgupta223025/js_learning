6e7ae642929dd8ea77414f11af2c2f2a965820ea
amit <amit.gupta223025@gmail.com>
2026-06-22 23:19:59 +0530

    learning

---
+++ classesandoops/mathpi.js
@@ -0,0 +1,23 @@
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable:true,
+
+    orderchai: function(){
+        console.log('chai nhi bani');
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+Object.defineProperty(chai,'name',{
+    //writable: false,
+    enumerable:false  //true value converts innto false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+
+for (let[key, value]of Object.entries(chai)){
+    if (typeof value !=='function'){
+        console.log(`${key}:${value}`);
+    }
+}
